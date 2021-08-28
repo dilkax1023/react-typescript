203013f0ec63c918b9ab9ee5aeec7cf2481dc8fd
Ghani Hakim <ghani.ouighour@gmail.com>
2021-08-28 23:33:04 +0200

    comment parent comp in feature-1 branch

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import ReactDom from 'react-dom';
-import Parent from './props/Parent';
+// import Parent from './props/Parent';
 import { GuestList } from './state/GuestList';
 import { UserSearch } from './state/UserSearch';
 
 export default function App() {
 	return (
 		<>
-			<Parent />
+			{/* <Parent /> */}
 			<UserSearch />
 			<GuestList />
 		</>
